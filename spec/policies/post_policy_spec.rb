ac76844ce0c9662fe69995b3ee1c79d00828422f
Tomek Rabczak <tomek.rabczak@gmail.com>
2022-05-27 00:24:49 -0500

    Add basic policy spec for PostPolicy

--- spec/policies/post_policy_spec.rb
+++ spec/policies/post_policy_spec.rb
@@ -1,17 +1,19 @@
+require "rails_helper"
+
 describe PostPolicy do
   subject { described_class }
 
   permissions :update?, :edit? do
-    it "denies access if post is published" do
-      expect(subject).not_to permit(User.new(admin: false), Post.new(published: true))
+    it "denies access if user isn't author" do
+      post = create(:post)
+      other_user = create(:user, email: "other_user@example.com")
+
+      expect(subject).not_to permit(other_user, post)
     end
 
-    it "grants access if post is published and user is an admin" do
-      expect(subject).to permit(User.new(admin: true), Post.new(published: true))
-    end
-
-    it "grants access if post is unpublished" do
-      expect(subject).to permit(User.new(admin: false), Post.new(published: false))
+    it "grants access if user is author" do
+      post = create(:post)
+      expect(subject).to permit(post.user, post)
     end
   end
 end
